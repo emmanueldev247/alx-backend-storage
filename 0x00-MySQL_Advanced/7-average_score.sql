8a94dd28074019d4b58ec042b6455b7f7508d71f
Emmanuel Ademola <mailemmydee@gmail.com>
2024-10-19 12:39:40 +0000

    added: Average score

---
+++ 0x00-MySQL_Advanced/7-average_score.sql
@@ -0,0 +1,24 @@
+--  script that creates a stored procedure ComputeAverageScoreForUser that computes and store the average score for a student.
+
+DELIMITER //
+
+DROP PROCEDURE IF EXISTS ComputeAverageScoreForUser;
+CREATE PROCEDURE ComputeAverageScoreForUser(
+		IN user_id INT
+)
+BEGIN
+	DECLARE average_score INT;
+
+	SELECT AVG(score) INTO average_score
+	FROM corrections
+	WHERE corrections.user_id = user_id;
+
+	IF average_score IS NOT NULL THEN
+		UPDATE users
+		SET users.average_score = average_score
+		WHERE users.id = user_id;
+	END IF;
+END; //
+
+
+DELIMITER ;
