f6c239cba64595eb4af9be5c64ef8517c8f10698
Emmanuel Ademola <mailemmydee@gmail.com>
2024-10-19 18:09:37 +0000

    updated: entire query

--- 0x00-MySQL_Advanced/8-index_my_names.sql
+++ 0x00-MySQL_Advanced/8-index_my_names.sql
@@ -1,28 +1,3 @@
 --  script that creates an index idx_name_first on the table names and the first letter of name
 
-ALTER TABLE names
-ADD first_letter CHAR(1);
-
-DELIMITER //
-DROP TRIGGER IF EXISTS before_insert_names;
-CREATE TRIGGER before_insert_names
-BEFORE INSERT ON  names
-FOR EACH ROW
-BEGIN
-	SET NEW.first_letter = LEFT(NEW.name, 1);
-END; //
-DELIMITER ;
-
-
-
-DELIMITER //
-DROP TRIGGER IF EXISTS after_update_names;
-CREATE TRIGGER after_update_names
-BEFORE UPDATE ON names
-FOR EACH ROW
-BEGIN
-	SET NEW.first_letter = LEFT(NEW.name, 1);
-END; //
-DELIMITER ;
-
-CREATE INDEX idx_name_first ON names (first_letter);
+CREATE INDEX idx_name_first ON names (LEFT(name, 1));
