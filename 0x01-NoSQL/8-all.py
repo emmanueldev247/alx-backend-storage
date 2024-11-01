d9681e6829d3145df3439a04da5a03bba29e1626
Emmanuel Ademola <mailemmydee@gmail.com>
2024-11-01 10:43:00 -0700

    updated: documentation

--- 0x01-NoSQL/8-all.py
+++ 0x01-NoSQL/8-all.py
@@ -3,6 +3,17 @@
 
 
 def list_all(mongo_collection):
+    """
+        Lists all documents in a MongoDB collection.
+
+        Args:
+            mongo_collection: A PyMongo collection object.
+
+        Returns:
+            A list of dictionaries, where each dictionary represents
+            a document in the collection.
+            If the collection is empty, an empty list is returned.
+    """
     doc_list = []
     for doc in mongo_collection.find():
         doc_list.append(doc)
